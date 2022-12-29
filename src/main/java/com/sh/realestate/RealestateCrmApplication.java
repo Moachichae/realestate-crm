b9a25755d356efbb322f877b112d2b8598588bd3
채승호 <chaeseungho@chaeseunghoui-MacBookPro.local>
2022-12-30 00:01:46 +0900

    [기타] 1. 프로젝트 생성.

---
+++ src/main/java/com/sh/realestate/RealestateCrmApplication.java
@@ -0,0 +1,13 @@
+package com.sh.realestate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealestateCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealestateCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sh/realestate/RealestateCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.realestate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealestateCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
